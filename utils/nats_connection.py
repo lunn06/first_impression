6e44e476d76f83ef9e51af38a19b8aac62e94f2d
dcdnc <kuunii06@gmail.com>
2024-09-21 04:02:04 +0700

    Merge remote-tracking branch 'origin/dev' into dev

--- utils/nats_connection.py
+++ utils/nats_connection.py
@@ -7,8 +7,8 @@ async def connect_to_nats(servers: list[str]) -> tuple[Client, JetStreamContext]
     nc: Client = await nats.connect(
         servers,
         connect_timeout=10,
-        reconnect_time_wait=5,
+        reconnect_time_wait=1,
     )
-    js: JetStreamContext = nc.jetstream()
+    js: JetStreamContext = nc.jetstream(timeout=20)
 
     return nc, js
